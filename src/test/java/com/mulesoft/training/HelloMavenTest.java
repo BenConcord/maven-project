dc9f67cc2ae101446cc99c0010195d1a4a7600bf
Ben MacKinney <ben.mackinney@concordusa.com>
2017-07-20 09:33:13 -0500

    added error test

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,13 +2,20 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
+	
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
 
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println("\n\nDynamic Port in Test case -1" + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
@@ -16,6 +23,13 @@ public class HelloMavenTest extends FunctionalTestCase {
     protected String[] getConfigFiles() {
     	String files[] = {"maven-project.xml", "global.xml"};
         return files;
+    }
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      System.out.println("\n\nDynamic Port in Test case -2" + dynamicPort.getNumber() + "\n\n");
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
 
 }
